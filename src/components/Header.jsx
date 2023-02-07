1e1f290c53df60bc2324089ba7662efb8bf4b1a6
Kane Milligan <kane.milligan@glean.co>
2023-02-07 08:52:35 +0000

    made vote count bold

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 import pageBanner from "../assets/images/pageBanner.png";
 
 const Header = () => {
+
   return (
     <div>
       <Link to="/reviews" className="header-link">
         <h1 className="header">NC Games</h1>
       </Link>
       <div className="divider"></div>
-      {<img src={pageBanner} alt="retro" className="page-banner" />}
+      <img src={pageBanner} alt="retro" className="page-banner" />
     </div>
   );
 };
