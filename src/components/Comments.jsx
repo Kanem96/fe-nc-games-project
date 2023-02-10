5acef71627c9e9e6d198ab5a648fec46baed85ff
Kane Milligan <kane.milligan@glean.co>
2023-02-10 11:41:44 +0000

    added comment form structure

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -4,6 +4,7 @@ import CommentCard from "./CommentCard";
 
 const Comments = ({ reviewId, commentsList, setCommentsList }) => {
   const [commentCount, setCommentCount] = useState(0);
+  const [isFocused, setIsFocused] = useState(false);
   useEffect(() => {
     const fetchComments = async () => {
       const comments = await getComments(reviewId);
@@ -16,6 +17,29 @@ const Comments = ({ reviewId, commentsList, setCommentsList }) => {
   return (
     <>
       <p className="comments-container-header">{commentCount} Comments</p>
+      <form action="" className="form-container">
+        <input
+          type="text"
+          className="comment-text-box"
+          onFocus={() => setIsFocused(true)}
+          placeholder="What do you think ?"
+        />
+        {isFocused ? (
+          <section className="submit-container">
+            <label className="username-label" htmlFor="username-input">
+              Username:{" "}
+            </label>
+            <input
+              name="username-input"
+              type="text"
+              className="username-text-box"
+            ></input>
+            <button>Submit</button>
+          </section>
+        ) : (
+          <div></div>
+        )}
+      </form>
       <ul className="comments-container">
         {commentsList.map((comment) => {
           return (
